a19752dc397f055aeae06453106c53ef82d7d455
Connor Cowan <ccowan003@comcast.net>
2021-09-16 16:31:46 -0700

    Initial CSS

--- src/components/GroceriesList.js
+++ src/components/GroceriesList.js
@@ -1,8 +1,6 @@
 // import React, {useEffect, useState} from 'react';
 
 function GroceriesList({ ingredient }) {
-    // console.log("IN GROCERIESLIST", ingredient)
-    // console.log("!!!!RECIPE INFO", ingredient.recipe.recipe_chosen)
     return (
         <li>{ingredient.quantity} x {ingredient.name}</li>
     );
